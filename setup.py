b0bbdfcc994c13a0c47a52843820d8812f64a3df
Eivind Fonn <evfonn@gmail.com>
2020-03-11 21:10:06 +0100

    Bump version to 1.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(Path(__file__).parent / 'README.rst') as f:
 
 setup(
     name='LRSplines',
-    version='1.1.0',
+    version='1.2.0',
     description='Python bindings for the LRSplines library',
     long_description_content_type='text/x-rst',
     long_description=desc,
