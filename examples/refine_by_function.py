fcd109742857b59f4dade96f1172c6cc6d29f498
Kjetil Johannessen <kjetijo@gmail.com>
2021-01-07 15:43:50 +0100

    Bugfix: missing controlpoint input when reading from file

--- examples/refine_by_function.py
+++ examples/refine_by_function.py
@@ -1,8 +1,11 @@
 from splipy.io import G2
+from splipy import curve_factory
+from splipy import surface_factory
 import lrspline as lr
 import sys
 import numpy as np
 import inspect
+import matplotlib.pyplot as plt
 
 def set_max_meshsize(myspline, amount):
 
@@ -61,35 +64,67 @@ def set_max_meshsize(myspline, amount):
 
 
 ### USER DEFINED REFINEMENT FUNCTION
-def ref(u):
-    return u + 0.025
+def ref(x,y):
+    return np.abs(y-x)+0.025
 
 
 # read model from file
 with G2(sys.argv[1]) as myfile:
-    mymodel = myfile.read()
+   mymodel = myfile.read()
+
+
+### make an example with controlpoint values
+# crv1 = curve_factory.circle_segment(theta=np.pi/2, r=1).rebuild(p=3,n=6)
+# crv2 = curve_factory.polygon([2,0], [2,2], [0,2])
+# srf = surface_factory.edge_curves(crv1, crv2)
+# srf.raise_order(0,1)
+# srf.refine(0,3)
+#
+# mymodel = [srf]
+# # write model to file
+# with G2('plate_with_hole.g2') as myfile:
+#    myfile.write(srf)
+
 
 # convert all patches to LRSpline representations
 lr_model = []
 for spline in mymodel:
+    cp = spline.controlpoints
+    cp = np.reshape(cp, (np.prod(cp.shape[:-1]),cp.shape[-1]), order='F')
+    print(cp)
     if spline.pardim == 2:
-        lr_model.append(lr.LRSplineSurface(spline.shape[0], spline.shape[1], spline.order(0), spline.order(1), spline.knots(0, True), spline.knots(1, True)))
+        lr_model.append(lr.LRSplineSurface(spline.shape[0], spline.shape[1], spline.order(0), spline.order(1), spline.knots(0, True), spline.knots(1, True), cp))
     elif spline.pardim == 3:
-        lr_model.append(lr.LRSplineVolume(spline.shape[0], spline.shape[1], spline.shape[2], spline.order(0), spline.order(1), spline.order(2), spline.knots(0, True), spline.knots(1, True), spline.knots(2, True)))
+        lr_model.append(lr.LRSplineVolume(spline.shape[0], spline.shape[1], spline.shape[2], spline.order(0), spline.order(1), spline.order(2), spline.knots(0, True), spline.knots(1, True), spline.knots(2, True), cp))
+    print([b.controlpoint for b in lr_model[-1].basis])
 
 # refine accordint to specifications
 for i, lrs in enumerate(lr_model):
     print(lrs)
     set_max_meshsize(lrs, ref)
 
-    if lrs.pardim == 2: # dump results from surface refinement
-        filename = f'patch_{i}.eps'
-        print(f'Writing mesh to {filename}')
-        with open(filename, 'wb') as myfile:
-            lrs.write_postscript(myfile)
+    # for SURFACE cases
+    if lrs.pardim == 2: 
+        ### dump results from refinement to eps file
+        # filename = f'patch_{i}.eps'
+        # print(f'Writing mesh to {filename}')
+        # with open(filename, 'wb') as myfile:
+        #    lrs.write_postscript(myfile)
+
+        # create a matplotlib figure of the mesh results
+        for m in lrs.meshlines:
+            t = np.linspace(*m.span(m.variable_direction), 40)
+            if m.constant_direction == 0:
+                x = np.array([lrs(m.value,t0) for t0 in t])
+            else:
+                x = np.array([lrs(t0,m.value) for t0 in t])
+            plt.plot(x[:,0],x[:,1], 'b-')
 
     filename = f'patch_{i}.lr'
     print(f'Writing result to {filename}')
     with open(filename, 'wb') as myfile:
         lrs.write(myfile)
+
+plt.axis('equal')
+plt.show()
 
