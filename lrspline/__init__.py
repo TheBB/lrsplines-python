2cd7db72dd41eee0d24556c574946f8b85354976
Eivind Fonn <evfonn@gmail.com>
2025-03-25 21:58:39 +0100

    Bump version: 1.14.0 -> 1.14.1

--- lrspline/__init__.py
+++ lrspline/__init__.py
@@ -27,7 +27,7 @@ from typing_extensions import Unpack
 
 from . import raw
 
-__version__ = "1.14.0"
+__version__ = "1.14.1"
 
 L = TypeVar("L", bound="LRSplineObject")
 R = TypeVar("R", bound=raw.LRSplineObject)
